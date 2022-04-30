90826e8347391ba17b45c2cc0c6124b0ad4af51a
Muhammed Yusuf <myusuf2121@gmail.com>
2022-05-01 00:20:29 +0300

    Refactored reveal to select and add class to different objects.

--- scripts/index.js
+++ scripts/index.js
@@ -24,26 +24,40 @@
 // document.addEventListener("scroll", frameSelect);
 // // window.onload(frameSelect);
 
-function reveal() {
-  //   const frames = [...document.querySelectorAll("iframe")];
-  const background = document.querySelector(".bg-layer");
-  const frames = [
-    ...document.querySelector(".objects-wrapper").children,
-    background,
-  ];
-  //   console.log(wrapper);
-  for (let i = 0; i < frames.length; i++) {
+function addClass(objectsArray, name, elementVisible = 500) {
+  const objects = Array.from(objectsArray);
+
+  for (let i = 0; i < objects.length; i++) {
     let scrollValue = window.scrollY;
-    const elementVisible = 500;
+    // const elementVisible = 500;
     if (scrollValue < elementVisible) {
-      //       frame.classList.add("is-visible");
-      frames[i].classList.add("is-visible");
-      frames[i].classList.remove("is-hidden");
+      objects[i].classList.add(`is-visible-${name}`);
+      objects[i].classList.remove(`is-hidden-${name}`);
     } else {
-      frames[i].classList.remove("is-visible");
-      frames[i].classList.add("is-hidden");
+      objects[i].classList.remove(`is-visible-${name}`);
+      objects[i].classList.add(`is-hidden-${name}`);
     }
   }
+}
+function reveal() {
+  //   const frames = [...document.querySelectorAll("iframe")];
+  const background = [document.querySelector(".bg-layer")];
+  const frames = [...document.querySelector(".objects-wrapper").children];
+  addClass(frames, "frame");
+  addClass(background, "background", 300);
+  //   console.log(wrapper);
+  // for (let i = 0; i < frames.length; i++) {
+  //   let scrollValue = window.scrollY;
+  //   const elementVisible = 500;
+  //   if (scrollValue < elementVisible) {
+  //     //       frame.classList.add("is-visible");
+  //     frames[i].classList.add("is-visible");
+  //     frames[i].classList.remove("is-hidden");
+  //   } else {
+  //     frames[i].classList.remove("is-visible");
+  //     frames[i].classList.add("is-hidden");
+  //   }
+  // }
 }
 
 window.addEventListener("scroll", reveal);
