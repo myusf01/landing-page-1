cb43d7a3b2768b7c6e3ccbd6275b156aa4facba9
Muhammed Yusuf <myusuf2121@gmail.com>
2022-04-30 15:57:40 +0300

    Migrate to pug.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,18 @@ const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const browserSync = require("browser-sync").create();
 const autoprefixer = require("gulp-autoprefixer");
+const pug = require("gulp-pug");
+
+function watch() {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+    },
+  });
+  gulp.watch("./views/**/*.pug", html);
+  gulp.watch("./styles/scss/**/*.scss", style);
+  gulp.watch("./scripts/**/*.js", browserSync.reload);
+}
 
 function style() {
   return gulp
@@ -12,15 +24,24 @@ function style() {
     .pipe(browserSync.stream());
 }
 
-function watch() {
-  browserSync.init({
-    server: {
-      baseDir: "./",
-    },
-  });
-  gulp.watch("./styles/scss/**/*.scss", style);
-  gulp.watch("./*.html").on("change", browserSync.reload);
-  gulp.watch("./scripts/**/*.js").on("change", browserSync.reload);
+function html() {
+  return gulp
+    .src("views/*.pug")
+    .pipe(pug())
+    .pipe(gulp.dest("./"))
+    .on("end", browserSync.reload);
 }
+
+// function watch() {
+//   browserSync.init({
+//     server: {
+//       baseDir: "./",
+//     },
+//   });
+//   gulp.watch("./styles/scss/**/*.scss", style);
+//   gulp.watch("./*.html").on("change", browserSync.reload);
+//   gulp.watch("./scripts/**/*.js").on("change", browserSync.reload);
+// }
 exports.style = style;
+exports.html = html;
 exports.watch = watch;
