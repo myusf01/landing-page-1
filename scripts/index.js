57a7e7211c6a9ed0bc5fbcacf13cb00ecf0119b1
Muhammed Yusuf <myusuf2121@gmail.com>
2022-05-02 23:57:49 +0300

    fix animation.

--- scripts/index.js
+++ scripts/index.js
@@ -13,12 +13,7 @@ function addClassOnScroll(objectsArray, name, elementVisible = 500) {
     }
   }
 }
-function reveal() {
-  const background = [document.querySelector(".bg-layer")];
-  const frames = [...document.querySelector(".objects-wrapper").children];
-  addClassOnScroll(frames, "frame");
-  addClassOnScroll(background, "background", 300);
-}
+
 function dotCreator(imageList = []) {
   const wrapper = document.querySelector(".dots");
   for (let index = 0; index < imageList.length - 2; index++) {
@@ -30,41 +25,50 @@ function dotCreator(imageList = []) {
   wrapper.children[0].classList.add("activeDot");
 }
 
+function toggleClassName(object, className, indexToAdd, indexToRemove) {
+  object[indexToAdd].classList.add(className);
+  object[indexToRemove].classList.remove(className);
+}
+function reveal() {
+  const background = [document.querySelector(".bg-layer")];
+  const frames = [...document.querySelector(".objects-wrapper").children];
+  addClassOnScroll(frames, "frame");
+  addClassOnScroll(background, "background", 300);
+}
+
 function slider() {
   const showcaseCarousel = document.querySelector(".carousel-slide");
   const showcaseImages = document.querySelectorAll(".carousel-slide img");
-  dotCreator(showcaseImages);
-  const dotIcons = document.querySelectorAll("i.bi-dot");
-  const dots = document.querySelector(".dots").children;
-  console.log(dotIcons, dots);
   const prevButton = document.querySelector("#prevButton");
   const nextButton = document.querySelector("#nextButton");
 
+  dotCreator(showcaseImages);
+  const dotIcons = document.querySelectorAll("i.bi-dot");
+
   let counter = 1;
   const imageSize = showcaseImages[0].clientWidth;
-
   showcaseCarousel.style.transform = `translateX(${-imageSize * counter}px`;
 
+  // next button
   nextButton.addEventListener("click", function () {
+    console.log(imageSize);
+
     if (counter >= showcaseImages.length - 1) return;
     if (counter < dotIcons.length) {
       // add class to current dot and remove previous one
-
-      dotIcons[counter].classList.add("activeDot");
-      dotIcons[counter - 1].classList.remove("activeDot");
+      toggleClassName(dotIcons, "activeDot", counter, counter - 1);
     }
     showcaseCarousel.style.transition = "transform 0.4s ease-in-out";
     counter++;
     showcaseCarousel.style.transform = `translateX(${-imageSize * counter}px`;
   });
-
+  // previous button
   prevButton.addEventListener("click", function () {
     if (counter <= 0) return;
     if (counter < dotIcons.length) {
+      console.log(counter);
       // add class to current dot and remove previous one
-
-      dotIcons[counter - 1].classList.add("activeDot");
-      dotIcons[counter].classList.remove("activeDot");
+      toggleClassName(dotIcons, "activeDot", counter - 1, counter);
     }
 
     showcaseCarousel.style.transition = "transform 0.4s ease-in-out";
@@ -79,8 +83,7 @@ function slider() {
       showcaseCarousel.style.transform = `translateX(${-imageSize * counter}px`;
 
       // add class to latest dot and remove from first dot
-      dotIcons[0].classList.remove("activeDot");
-      dotIcons[counter - 1].classList.add("activeDot");
+      toggleClassName(dotIcons, "activeDot", counter - 1, 0);
     }
     if (showcaseImages[counter].id === "firstClone") {
       showcaseCarousel.style.transition = "none";
@@ -88,8 +91,10 @@ function slider() {
       showcaseCarousel.style.transform = `translateX(${-imageSize * counter}px`;
 
       // add class to first dot and remove from latest dot
-      dotIcons[counter - 1].classList.add("activeDot");
-      dotIcons[dotIcons.length - 1].classList.remove("activeDot");
+      toggleClassName(dotIcons, "activeDot", counter - 1, dotIcons.length - 1);
+      console.log("first");
+      // dotIcons[counter - 1].classList.add("activeDot");
+      // dotIcons[dotIcons.length - 1].classList.remove("activeDot");
     }
   });
 }
